64adde35b88243f9fd169530b1b4ca0f1ab344df
Dhairya Majmudar <2022kuec2045@iiitkota.ac.in>
2024-03-30 10:26:08 +0530

    adding hover effect

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,10 @@ const Footer = () => {
     <div className="max-w-2xl mx-auto px-4 text-center font-semibold text-xl my-5 mt-10">
       Created with ♥️ By
       <Link href={"https://dhaiyra-majmudar.netlify.app/"} target="_blank">
-        <span className="text-primary"> Dhairya Majmudar</span>
+        <span className="text-primary hover:text-blue-800 dark:hover:text-blue-600">
+          {" "}
+          Dhairya Majmudar
+        </span>
       </Link>
     </div>
   );
