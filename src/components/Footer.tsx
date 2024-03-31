9bfad95d9c1a3eda310b202ed1524b4cc6febb6b
Dhairya Majmudar <2022kuec2045@iiitkota.ac.in>
2024-03-31 20:58:00 +0530

    footer fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <div className="max-w-2xl mx-auto px-4 text-center font-semibold text-xl my-5 mt-10">
-      Created with ♥️ By
+      Created with ♥️ By{" "}
       <Link href={"https://dhaiyra-majmudar.netlify.app/"} target="_blank">
         <span className="text-primary hover:text-blue-800 dark:hover:text-blue-600">
-           Dhairya Majmudar
+          Dhairya Majmudar
         </span>
       </Link>
     </div>
